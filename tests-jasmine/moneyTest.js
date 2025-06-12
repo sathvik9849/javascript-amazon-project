1701413e9990e9c7aebd45999ecd69e2d35aa954
sathvik9849 <dsathvikreddy123@gmail.com>
2025-06-12 13:07:55 -0400

    Added Jasmine testing framework

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,12 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+describe("Test Suite: Format Currency", ()=>{
+    it('Base Case', () =>{
+        expect(formatCurrency(2095)).toEqual('20.95')
+    })
+    it('works with 0', () =>{
+        expect(formatCurrency(0)).toEqual('0.00')
+    })
+    it('rounds up to nearest cent', ()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01')
+    })
+})
