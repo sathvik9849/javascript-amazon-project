b6c9abc74dda40b33f6bd9427c0111d5c6d15504
sathvik9849 <dsathvikreddy123@gmail.com>
2025-06-12 16:08:12 -0400

    created automated Tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
+export function loadStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
 
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: 2,
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId: 2,
-    },
-  ];
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: 2,
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: 2,
+      },
+    ];
+  }
 }
 function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -54,15 +57,15 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
       matchedProduct = cartItem;
     }
   });
-  matchedProduct.deliveryOptionId = deliveryOptionId
-  saveToStorage()
+  matchedProduct.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
 }
 
-export function updateProductQuantity(quantity, productId){
-    cart.forEach((cartItem)=>{
-      if(cartItem.productId == productId){
-        cartItem.quantity = quantity
-      }
-    })
-    saveToStorage()
-  }
+export function updateProductQuantity(quantity, productId) {
+  cart.forEach((cartItem) => {
+    if (cartItem.productId == productId) {
+      cartItem.quantity = quantity;
+    }
+  });
+  saveToStorage();
+}
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 describe("Test Suite: Format Currency", ()=>{
     it('Base Case', () =>{
         expect(formatCurrency(2095)).toEqual('20.95')
